fb8b21b42fee9f651536dfa1a3138db9e3c2aa32
autoshgame <66913909+autoshgame@users.noreply.github.com>
2021-06-07 17:17:52 +0700

    Add two cameras, press w to switch camera, add more obstacles and widen the road

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class CameraControler : MonoBehaviour
 {
     public GameObject player;
+    public Camera camera1;
+    public Camera camera2;
     private float xStable = 0f;
+    private bool keypress = true;
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = player.transform.position + new Vector3(0, 7, -40);
+        camera1.enabled = true;
+        camera2.enabled = false;
         xStable = player.transform.position.x;
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W) && keypress == true)
+        {
+            camera1.enabled = !camera1.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+    }
     void LateUpdate()
     {
-        this.transform.position = new Vector3(xStable, player.transform.position.y + 7, player.transform.position.z - 40);
+        camera1.transform.position = new Vector3(xStable, player.transform.position.y + 6, player.transform.position.z - 30);
+        camera2.transform.position = player.transform.position + new Vector3(0, 3, -5);
     }
 }
